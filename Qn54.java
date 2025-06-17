4e5b4491178bd1a4c3fea83b0ae4f40ae48c8a8b
Karthikeyan R <karthiktonystark00@gmail.com>
2025-06-17 17:14:46 +0530

    today's work

---
+++ Qn54.java
@@ -0,0 +1,17 @@
+package com.ZohoQns;
+
+public class Qn54 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == j || j == n - i - 1){
+                    System.out.print(j+1);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Qn55.java
@@ -0,0 +1,21 @@
+//package com.ZohoQns;
+//
+//public class Qn55 {
+//    public static void main(String[] args) {
+//        int n = 4;
+//        int row = 2 * n-1;
+//        int k = 0;
+//        for(int i = 0;i<row;i++){
+//            for(int j = 0;j<row;j++){
+//                if(i==k || j==k ||i==row-k-1 || j ==row-k-1){
+//                    System.out.print(n);
+//                }
+//                else{
+//
+//                }
+//            }
+//            n--;
+//            System.out.println();
+//        }
+//    }
+//}
